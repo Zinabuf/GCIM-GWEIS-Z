0802572d9c27706e80f71bcb2365685295c7740e
Zinabu Fentaw WERASHE <103751083+Zinabuf@users.noreply.github.com>
2026-04-03 12:38:29 +1030

    Delete Data directory

--- Data/Binary_outcome/gcim_gweis.sh
+++
@@ -1,8 +0,0 @@
-#Binary data analysis
-for  ((i=1;i<=1;i++))
-    	do
-		R CMD BATCH --no-save gweis_p.R
-		R CMD BATCH --no-save gweis_r.R
-		R CMD BATCH --no-save gcim_gweis_p.R
-		R CMD BATCH --no-save gcim_gweis_r.R
-	done
